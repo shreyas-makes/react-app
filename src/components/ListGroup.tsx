dd847375a7d7f08b005becebf486abe6019bbd32
Shreyas <shreyas314159@gmail.com>
2024-08-11 13:56:34 +0100

    added a header to the list of items
    Usually it is difficult since a react component can only render one type of element, to make this possible, added a Fragment under an empty nested bracket to indicate that

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,18 @@
+import { Fragment } from "react";
+
 function ListGroup(){
-    return <ul className="list-group">
-    <li className="list-group-item">An item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
-  </ul>
+    return(
+  <>
+          <h1>hello</h1>    
+          <ul className="list-group">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+            <li className="list-group-item">A fourth item</li>
+            <li className="list-group-item">And a fifth one</li>
+          </ul>
+  </> 
+) 
 }
 
 export default ListGroup;
